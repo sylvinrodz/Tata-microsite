a8047ff2bda458583e10834a099dca4b460452b5
Sylvin Rodrigues <sylvin@skilliza.com>
2020-12-11 00:53:23 +0530

    tatamicrosite2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,14 +9,18 @@ import { Component, ElementRef, ViewChild, Inject } from '@angular/core';
 export class MainComponent  {
   @ViewChild('videoPlayer') videoplayer: ElementRef;
   @ViewChild('videoPlayer1') videoplayer1: ElementRef;
+  @ViewChild('btn') btn: ElementRef;
   constructor(@Inject(DOCUMENT) document) { 
    console.log( document.getElementById('video'))
   }
   toggleVideo(event: any) {
     this.videoplayer.nativeElement.play();
 }
+vidEnded(){
+  this.btn.nativeElement.style.display = "block";
+}
 PlayVideo(){
-  console.log("run")
+  this.btn.nativeElement.style.display = "none";
   this.videoplayer.nativeElement.style.display = "none";
   this.videoplayer1.nativeElement.style.display = "block";
   this.videoplayer1.nativeElement.play();
